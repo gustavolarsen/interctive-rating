9b24785671e3a03c614b31ffcaf294b900c3a54a
Gustavo Larsen <guslarsen@gmail.com>
2023-06-07 13:51:36 -0300

    refactor: Adjusts Logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,12 @@
+import StarImg from '../assets/icon-star.svg';
 export const Logo = () => {
   return (
-    <div className="bg-gray-700 w-12 h-12 rounded-full bg-icon-star bg-no-repeat bg-center"></div>
+    <div className="bg-gray-700 w-12 h-12 rounded-full flex items-center justify-center">
+      <img
+        className="self-center justify-self-center"
+        src={StarImg}
+        alt="Logo orange star"
+      />
+    </div>
   );
 };
